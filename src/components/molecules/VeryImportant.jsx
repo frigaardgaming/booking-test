bd982d337e84fd406485a2358ceb4241131ecebc
Oliver Jeppesen <oliverjeppesen123@icloud.com>
2023-11-08 15:00:26 +0100

    Oprettelse af urls og css module

---
+++ src/components/molecules/VeryImportant.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from "./VeryImportant.module.css"
+
+export default function VeryImportant() {
+  return (
+    <div className={styles.container}>VeryImportant</div>
+  )
+}
---
+++ src/pages/booking/fag.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function fag() {
+  return (
+    <div>fag</div>
+  )
+}
